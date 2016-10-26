1b2563d1a41a81dae28cd7a36d16a3c9ab6fd2b1
ESLLO <master@esllo.com>
2016-10-26 19:02:04 +0900

    ADD

---
+++ src/com/esllo/ch/CV.java
@@ -0,0 +1,12 @@
+package com.esllo.ch;
+
+import static org.opencv.core.Core.NATIVE_LIBRARY_NAME;
+
+public class CV {
+
+	static {
+		System.loadLibrary(NATIVE_LIBRARY_NAME);
+	}
+	
+	
+}
---
+++ src/com/esllo/ch/ColorHistogram.java
@@ -0,0 +1,7 @@
+package com.esllo.ch;
+
+public class ColorHistogram {
+
+	public ColorHistogram() {
+	}
+}
